9dd33598c65559b5ce3d1aafd5973f5b4f5020ae
Sarah DeBey <sarah.debey@gmail.com>
2020-09-26 16:06:44 -0700

    i hate css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import './index.css'
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button>
+        {'button'}
+      </button>
+    )
+  }
+}
+
+class Board extends React.Component {
+  render() {
+    return (
+      <div className="square">
+        <Square/>
+      </div>
+    )
+  }
+}
 
 class Game extends React.Component {
-  
+  render() {
+    return (
+        <div className="board">
+          <Board/>
+        </div>
+    )
+  }
 }
 
 ReactDOM.render(
