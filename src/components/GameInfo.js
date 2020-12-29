c58c1667bf365bd0ae11224978cdb6aa5ad9bd17
Sarah DeBey <sarah.debey@gmail.com>
2020-12-28 19:19:36 -0800

    creates separate Winner component for displaying winner. Uses useState to create winner and setWinner to keep track of whether game is over, and renders the Winner component conditionally

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -2,32 +2,16 @@ import React from 'react';
 
 const GameInfo = props => {
 
-
-  const declareWinner = () => {
-    let winner;
-    if (props.playerA.matches > props.playerB.matches) {
-      winner = `The Winner is: ${props.playerA.name}`
-    } else if (props.playerB.matches > props.playerA.matches) {
-      winner = `The Winner is: ${props.playerB.name}`
-    } else {
-      winner = 'DRAW';
-    }
-    return winner;
-  }
-
-    return (
-      <div>
-        <button onClick = {props.fillSquares}>Begin Game</button>
-          <ul>
-            <li>{`Next Player: ${props.playerA.name}`}</li>
-            <li>{`${props.playerA.name}'s matches: ${props.playerA.matches}`}</li>
-            <li>{`${props.playerB.name}'s matches: ${props.playerB.matches}`}</li>
-          </ul>
-          <div className="winner">
-            {props.exposedSquares.length === props.gameSquares.length && props.gameSquares.length > 0 ? declareWinner() : null}
-          </div>
-        </div>
-      )
+  return (
+    <div>
+      <button onClick = {props.fillSquares}>Begin Game</button>
+        <ul>
+          <li>{`Next Player: ${props.playerA.name}`}</li>
+          <li>{`${props.playerA.name}'s matches: ${props.playerA.matches}`}</li>
+          <li>{`${props.playerB.name}'s matches: ${props.playerB.matches}`}</li>
+        </ul>
+      </div>
+    )
 }
 
 export default GameInfo;
