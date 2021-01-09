7411ee8e9d4d91bf289803468c99f95b5436580e
Sarah DeBey <sarah.debey@gmail.com>
2021-01-08 16:28:00 -0800

    moves test into Square component subdirectory and has a passing snapshot test

--- src/components/Square/Square.snapshot.test.js
+++ src/components/Square/Square.snapshot.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Square from '../components/Square.js';
+import Square from './Square';
 
 //snapshot tests just make sure that your UI doesn't change unexpectedly during development. Should also still write unit and functional tests, to verify that the application behaves as it should, not just that it hasn't changed
 test('Image tag returned if props.showImage is true, null returned if props.showImage is false', () => {
