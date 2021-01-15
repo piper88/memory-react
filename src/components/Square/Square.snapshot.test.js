6c0ad550b79baf3b9a1cd844f085218183a200ae
Sarah DeBey <sarah.debey@gmail.com>
2021-01-15 15:14:22 -0800

    fixes square snapshot tests to toggle between true and false of showImage prop

--- src/components/Square/Square.snapshot.test.js
+++ src/components/Square/Square.snapshot.test.js
@@ -3,16 +3,31 @@ import renderer from 'react-test-renderer';
 import Square from './Square';
 
 //snapshot tests just make sure that your UI doesn't change unexpectedly during development. Should also still write unit and functional tests, to verify that the application behaves as it should, not just that it hasn't changed
-test('should render Square component correctly, with showImage prop equal to true as well as false', () => {
+test('should render Square component correctly, with showImage prop equal to true', () => {
   const component = renderer.create(
     <Square
      showImage = {true}
      onClick = {() => 'fake callback'}
     />
   )
+  //tree is just what's returned from the component
   let tree = component.toJSON();
+  console.log('tree when true');
+  console.log(tree);
   expect(tree).toMatchSnapshot();
 
-  tree.props.showImage = false
+})
+
+test('should render Square component correctly, with showImage prop equal to false', () => {
+  const component = renderer.create(
+    <Square
+    showImage={false}
+    onClick = {() => 'fake callback'}
+    />
+  )
+
+  let tree = component.toJSON();
+  console.log('tree when false');
+  console.log(tree);
   expect(tree).toMatchSnapshot();
 })
