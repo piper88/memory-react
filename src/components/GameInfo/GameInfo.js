4ef75e4645ea76ac8e2955df6fdbb433c68824e7
Sarah DeBey <sarah.debey@gmail.com>
2021-01-19 20:23:25 -0800

    submitting player names now begins game, removes showForm state in playerform since game keeps track of showForm state

--- src/components/GameInfo/GameInfo.js
+++ src/components/GameInfo/GameInfo.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import './gameinfo.css';
 
 const GameInfo = props => {
-  const beginGame = props.gameSquares.length > 0 ? null : <button onClick = {props.fillSquares}>Begin Game</button>;
+  // const beginGame = props.gameSquares.length > 0 ? null : <button onClick = {props.fillSquares}>Begin Game</button>;
 
+// {beginGame}
   return (
     <div data-set="GameInfo Component">
 
-        {beginGame}
+
         <div>
           <h4>{`Next Player: ${props.playerA.name}`}</h4>
           <p>{`${props.playerA.name}'s matches: ${props.playerA.matches}`}</p>
--- src/components/PlayerForm/PlayerForm.js
+++ src/components/PlayerForm/PlayerForm.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 
 
 //A controlled component is one in which react controls the values of form inputs.
+//I believe it's necessary for this component to be stateful if you want it to be a controlled component.
+
+//If you want to use uncontrolled form (normally best to use controlled), meaning the DOM handles the data from the form, and you need access to the forms data, can use a ref (look it up)
 const PlayerForm = props => {
   const [playerA, setPlayerA] = useState('');
   const [playerB, setPlayerB] = useState('');
-  const [showForm, setShowForm] = useState(true);
 
-  //set the player's names
+  // set the player's names
   function handleChange(event) {
     let player = event.target;
     player.name ==='playerA' ? setPlayerA(player.value) : setPlayerB(player.value);
@@ -17,7 +19,6 @@ const PlayerForm = props => {
   function handleSubmit(event) {
     props.setPlayerNames(playerA, playerB);
     event.preventDefault();
-    setShowForm(false);
   }
 
   useEffect(() => {
@@ -26,35 +27,35 @@ const PlayerForm = props => {
     };
   }, []);
 
-  if (showForm) {
-    return (
-      <form onSubmit = {handleSubmit} data-test = "Player Form">
-        <label>
-          Player A:
-          <input
-          type="text"
-          name='playerA'
-          value = {playerA}
-          onChange = {handleChange.bind(this)}/>
-        </label>
-        <label>
-          Player B:
-          <input
-          type="text"
-          name='playerB'
-          value = {playerB}
-          onChange = {handleChange.bind(this)}/>
-        </label>
-        <button>
-          Submit
-        </button>
-      </form>
-    )
-  } else {
-    return (
-      null
-    )
-  }
+    //setting value on input means its a controlled component, and without an onChange handler, the input will be read only, unless you use a ref.
+    //value is what shows as the user types
+  return (
+    <form onSubmit = {handleSubmit} data-test = "Player Form">
+      <label>
+        Player A:
+        <input
+        type="text"
+        name='playerA'
+        value = {playerA}
+        placeholder = 'Enter player one here...'
+        onChange = {handleChange.bind(this)}
+        />
+      </label>
+      <label>
+        Player B:
+        <input
+        type="text"
+        name='playerB'
+        value = {playerB}
+        placeholder = 'Enter player two here...'
+        onChange = {handleChange.bind(this)}
+        />
+      </label>
+      <button>
+        Begin Game!
+      </button>
+    </form>
+  )
 }
 
 PlayerForm.propTypes = {
